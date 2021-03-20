ad03faf35350f1ab668c12cba52d1e006f66e5bc
Thai Thien <tthien@apcs.vn>
2021-03-20 09:44:27 +0700

    RTSP Camera to Image OK

---
+++ TryRTSP/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+
+namespace TryRTSP
+{
+    class Example
+    {
+        public async Task meowAsync() {
+            string output = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".mp4");
+            var mediaInfo = await FFmpeg.GetMediaInfo("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
+
+            var conversionResult = await FFmpeg.Conversions.New()
+                .AddStream(mediaInfo.Streams)
+                .SetOutput(output)
+                .Start();
+        }
+    }
+}
---
+++ TryRTSP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+
+namespace TryRTSP
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task task = convertVideoImageCameraOnvif();
+            task.Wait();
+
+        }
+
+
+        public static string buildName(int number)
+        {
+           string path =  "G://VSCsharp/tmp_out/fileNameNo" + number + ".png";
+           Console.WriteLine(path);
+           return path;
+        }
+
+  
+
+        public static async Task convertVideoImageCameraOnvif()
+        {
+            FFmpeg.SetExecutablesPath("C://ffmpeg/bin/");
+            var mediaInfo = await FFmpeg.GetMediaInfo("rtsp://admin:Aa123456@192.168.1.2:554/onvif1");
+            var cancellationTokenSource = new System.Threading.CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(10000);
+            System.Func<string, string> outputFileNameBuilder = (number) => { return "G://VSCsharp/tmp_out/onvif/fileNameNo" + number + ".png"; };
+            IConversionResult conversionResult = await FFmpeg.Conversions.New()
+                .AddStream(mediaInfo.Streams.First())
+                .ExtractEveryNthFrame(10, outputFileNameBuilder)
+                .Start(cancellationTokenSource.Token);
+        }
+
+    }
+}
